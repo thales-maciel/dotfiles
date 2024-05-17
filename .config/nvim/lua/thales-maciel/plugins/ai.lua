58b850d06a33c66d874025ad3fad9939076d444b
Thales Maciel <thales@thalesmaciel.com>
2024-05-17 13:38:05 -0300

    nvim tabnine config

--- .config/nvim/lua/thales-maciel/plugins/ai.lua
+++ .config/nvim/lua/thales-maciel/plugins/ai.lua
@@ -1,27 +1,17 @@
 return {
-    -- {
-    --     "codota/tabnine-nvim",
-    --     build = "./dl_binaries.sh",
-    --     config = function()
-    --         require("tabnine").setup({
-    --             disable_auto_comment=true,
-    --             accept_keymap="<Right>",
-    --             dismiss_keymap="<Left>",
-    --             debounce_ms=700,
-    --             suggestion_color = {gui="#808080", cterm=244},
-    --             exclude_filetypes = {"TelescopePrompt"},
-    --             log_file_path = nil,
-    --         })
-    --     end
-    -- },
     {
-        "sourcegraph/sg.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        build = "nvim -l build/init.lua",
-        config = function ()
-            require("sg").setup {
-
-            }
+        "codota/tabnine-nvim",
+        build = "./dl_binaries.sh",
+        config = function()
+            require("tabnine").setup({
+                disable_auto_comment=true,
+                accept_keymap="<Right>",
+                dismiss_keymap="<Left>",
+                debounce_ms=700,
+                suggestion_color = {gui="#808080", cterm=244},
+                exclude_filetypes = {"TelescopePrompt"},
+                log_file_path = nil,
+            })
         end
-    }
+    },
 }
